f4a01056f27766ba0422ed82323ccc15b760d72a
AhnByeongHo <qudgh7145@naver.com>
2021-09-24 18:35:11 +0900

    FIX: add login put temp data

--- src/sites/ondisk.py
+++ src/sites/ondisk.py
@@ -9,13 +9,15 @@ chromedriver_path = re.sub(r"automation_webhard/?.*", "automation_webhard/", chr
 chromedriver_path = chromedriver_path + "chromedriver"
 driver = webdriver.Chrome(chromedriver_path)
 
-driver.get("http://www.ondisk.co.kr/index.html")
+driver.get("http://www.ondisk.co.kr/index.php")
 
 time.sleep(1)
-id_element = driver.find_element_by_xpath(".//div[@class='page-login']/form/fieldset/div[@class='insert']/p[1]/input")
-password_element = driver.find_element_by_xpath(".//div[@class='page-login']/form/fieldset/div[@class='insert']/p[2]/input")
 
-driver.f
+print(driver.page_source)
+id_element = driver.find_element_by_xpath(".//div[@class='insert']/p[1]/input")
+id_element.send_keys("on")
+password_element = driver.find_element_by_xpath(".//div[@class='insert']/p[2]/input")
+password_element.send_keys("di")
 
 
 if __name__ == '__main__':
