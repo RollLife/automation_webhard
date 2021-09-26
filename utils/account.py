b745a6b554e5e56e32c40c666c797e91f77e6a2f
AhnByeongHo <qudgh7145@naver.com>
2021-09-26 22:12:20 +0900

    FIX: 계정 파일을 생성하지 않거나 기본값으로 자동으로 반영될수 있도록 수정

--- utils/account.py
+++ utils/account.py
@@ -2,7 +2,7 @@ import re
 import os
 import json
 
-ALLOW_SITE_NAME_LIST = ["ondisk", "filenori, filekuki", "wedisk"]
+ALLOW_SITE_NAME_LIST = ["ondisk", "filenori", "filekuki", "wedisk"]
 DEFAULT_ACCOUNT_VALUE = "$set_plz"
 
 ACCOUNT_FILE_PATH = os.getcwd()
@@ -19,3 +19,31 @@ if not os.path.exists(ACCOUNT_FILE_PATH):
     f = open(ACCOUNT_FILE_PATH, "w")
     json.dump(default_account_file, f)
     f.close()
+
+result_account_data = {}
+with open(ACCOUNT_FILE_PATH, "r") as ac:
+    current_account_data = json.load(ac)
+
+for site_name, account_dict in current_account_data.items():
+    if site_name not in ALLOW_SITE_NAME_LIST:
+        continue
+    if not account_dict:
+        result_account_data[site_name] = {"id": DEFAULT_ACCOUNT_VALUE,
+                                          "pw": DEFAULT_ACCOUNT_VALUE}
+    if not account_dict['id'] or not account_dict['pw']:
+        result_account_data["id"] = DEFAULT_ACCOUNT_VALUE
+        result_account_data["pw"] = DEFAULT_ACCOUNT_VALUE
+
+    result_account_data[site_name] = account_dict
+
+for site_name in ALLOW_SITE_NAME_LIST:
+    if site_name in result_account_data.keys():
+        continue
+    result_account_data[site_name] = {
+        "id": DEFAULT_ACCOUNT_VALUE,
+        "pw": DEFAULT_ACCOUNT_VALUE
+    }
+
+if current_account_data != result_account_data:
+    with open(ACCOUNT_FILE_PATH, "w") as f:
+        json.dump(result_account_data, f)
