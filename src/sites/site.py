06e1a9aed9198f79b5a4a00ea5d1193cdbfb2b67
AhnByeongHo <qudgh7145@naver.com>
2021-09-26 22:30:36 +0900

    FIX: chromedriver 처음 시동 기능 구현
    기존에는 같은 코드에 존재 했지만 따로 구분함으로 유지보수에 용이하도록 수정

--- src/sites/site.py
+++ src/sites/site.py
@@ -1,12 +1,22 @@
+import os
+import re
 import json
 
+from selenium import webdriver
+
 from utils.account import ACCOUNT_FILE_PATH, DEFAULT_ACCOUNT_VALUE
+
+IE_AGENT = "Mozilla/5.0 (MSIE 10.0; Windows NT 6.1; Trident/5.0)"
+
+chromedriver_path = os.getcwd()
+chromedriver_path = re.sub(r"automation_webhard/?.*", "automation_webhard/", chromedriver_path)
+chromedriver_path = chromedriver_path + "chromedriver"
 
 
 class Site:
     def __init__(self, site_name):
         self.site_name = site_name
-        self.load_account_info()
+        self.account = self.load_account_info()
 
     def load_account_info(self):
         with open(ACCOUNT_FILE_PATH, "r") as f:
@@ -17,3 +27,14 @@ class Site:
             if account_id == DEFAULT_ACCOUNT_VALUE or account_pw == DEFAULT_ACCOUNT_VALUE:
                 # TODO: must need to change logging module
                 raise
+        return {"id": account_id, "pw": account_pw}
+
+    def init_chrome_driver(self, chrome_driver_path, chrome_options=None):
+        if chrome_options:
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.add_argument(chrome_options)
+        driver = webdriver.Chrome(chrome_driver_path, chrome_options=chrome_options)
+        return driver
+
+    def run(self):
+        pass
