f51858fc838395850245a91a128ee75d2283a026
AhnByeongHo <qudgh7145@naver.com>
2021-09-29 00:24:15 +0900

    FIX: 정상적인 출석체크 페이지로 이동할 수 있도록 수정
    기존은 이벤트 페이지 자체에 접근을 했다면
    이번엔 출석체크 페이지 안에 있는 iframe으로 이동하여 룰렛을 작동 시킬수 있도록 수정함

--- src/sites/ondisk.py
+++ src/sites/ondisk.py
@@ -10,13 +10,11 @@ from selenium import webdriver
 from src.sites.sites import Site, IE_AGENT, chromedriver_path
 
 
-# TODO: alert 내용 확인하여 '오늘 이미 출석하셨습니다' 해당 내용이 나오면 완료 혹은 넘어가도록 하기
 # TODO: 테스트 구현 및 구조화 완료
 # TODO: 윈도우 환경에서 구동 확인
 # TODO: 모니터링 도구 및 백그라운드 실행 혹은 자동 실행 도구 필요
 # TODO: 로깅 모듈 추가
 # TODO: pylint 추가
-# TODO: issue 모두 정리
 
 
 class Ondisk(Site):
@@ -43,28 +41,12 @@ class Ondisk(Site):
         # 이벤트 기간중 alert 된 메시지가 발생했을 경우
         self.wait_for_alert_present(time=2)
 
-        self.browser.get(self.main_page)
+        # 출석체크 페이지
+        self.browser.get("https://ondisk.co.kr/index.php?mode=eventMarge&sm=event&action=view&idx=746&event_page=1")
 
-        # 결제 이벤트 팝업 해당 팝업을 닫아야 버튼이 눌림(페이지 전체 영역에 팝업이 발생)
-        try:
-            event_popup_close = WebDriverWait(self.browser, 3).until(
-                EC.presence_of_element_located((By.XPATH, ".//div[@id='js-charge-layer']/p[@class='btn_close']"))
-            )
-            event_popup_close.click()
-        except TimeoutException:
-            pass
-        except NoSuchElementException:
-            pass
-
-        # 출석체크
-        # check_button = browser.find_element_by_xpath(".//ul[@class='etc_button']/li[@class='check']")
-        # check_button.click()
-        #
-        # time.sleep(1)
-
-        # 출석체크 페이지를 클릭하여 이동이 되지 않아 강제 페이지 이동
-        self.browser.get(
-            "https://ondisk.co.kr/event/20140409_attend/event.php?mode=eventMarge&sm=event&action=view&idx=746&event_page=1")
+        # 출석체크 페이지는 iframe으로 구성되어있기때문에 iframe으로 불러와야한다.
+        iframe_event_page = self.wait_for_xpath_element_located(".//div[@id='page-contents']//iframe", 4)
+        self.browser.switch_to.frame(iframe_event_page)
 
         roulette_button = self.wait_for_xpath_element_located(".//div[@id='js-roulette']/p/button")
         roulette_button.click()
@@ -79,12 +61,13 @@ class Ondisk(Site):
             else:
                 print(alert.text)
             alert.accept()
-            # TODO: 이때 어떤 알림인지 알 수 있어야한다.
-            # 정상적으로 출석이 되었는지 아니면 출석이 이미 되었는지 판단의 재료가 되기 대문
 
         except TimeoutException:
             print("no alert")
 
+        # 정상 작동 확인용
+        self.browser.get(self.main_page)
+        time.sleep(3)
         self.browser.quit()
 
 
