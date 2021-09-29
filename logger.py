7e7d4ce6eb0460e5dab0411d8071c85d4984a08a
AhnByeongHo <qudgh7145@naver.com>
2021-09-30 07:37:13 +0900

    FIX: 파일로 로그 발생하는 부분 수정

--- logger.py
+++ logger.py
@@ -14,11 +14,6 @@ stream_handler = logging.StreamHandler()
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
 
-# log를 파일에 출력
-file_handler = logging.FileHandler('process.log')
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
-
 if __name__ == '__main__':
     # 'application' code
     logger.debug('debug message')
